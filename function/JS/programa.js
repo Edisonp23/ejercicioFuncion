09eda2ddb64f51c0b0eb5335f6086119b2a9a4b1
Edisonp23 <67557552+Edisonp23@users.noreply.github.com>
2020-09-22 09:33:08 -0500

    Add files via upload

---
+++ function/JS/programa.js
@@ -0,0 +1,8 @@
+function miFuncion2(nombre) {
+    alert ("hola"+ nombre + " desde función2");
+}
+
+function mostrarNombre ( parametro ) {
+    var nombre = parametro.value;
+    alert ("hola " + nombre + " desde mostrarNombre"); 
+}
